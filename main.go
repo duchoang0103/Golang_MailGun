f2a9c4a414404cf8f6fb99945ffb90e0c837e722
duchoang0103 <hoangkimanhduc01031999@gmail.com>
2022-08-05 10:07:12 +0700

    sửa khóa vì bị lộ

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 
 // Your available domain names can be found here:
 // (https://app.mailgun.com/app/domains)
-var yourDomain string = "sandbox1b4f79d8ff50472487ffdd638292d793.mailgun.org"
+var yourDomain string = "***sandbox1b4f79d8ff50472487ffdd638292d793.mailgun.org"
 
 //"sandboxb35142e463314da09b5baa7fd0878ffb.mailgun.org" // e.g. mg.yourcompany.com
 
 // You can find the Private API Key in your Account Menu, under "Settings":
 // (https://app.mailgun.com/app/account/security)
-var privateAPIKey string = "47c46f002b5121fb6408d78aab109051-1b3a03f6-07cd747d" //"210edf5ee4c770ebcba833e747bc5eea-1b3a03f6-5e0578d0"
+var privateAPIKey string = "***47c46f002b5121fb6408d78aab109051-1b3a03f6-07cd747d" //"210edf5ee4c770ebcba833e747bc5eea-1b3a03f6-5e0578d0"
 
 func main() {
 	// Create an instance of the Mailgun Client
